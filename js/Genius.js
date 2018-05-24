f81f6f0ee7b07a01776cbafc5dec893062f8c745
MiniRaccoonDog <atorres@spoonflower.com>
2018-05-24 14:08:02 -0400

    fixed genius conflicts

--- js/Genius.js
+++ js/Genius.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-function test() {
-    return "This is a test"
+function test () {
+  return 'This is a test'
 }
-=======
 function screaming () {
   window.alert('AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!')
 }
->>>>>>> refs/remotes/origin/genius
